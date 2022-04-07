a3cc62102e5bc24e527ef68ca20eaf14a72fffce
its0beno <boodebeno@gmail.com>
2022-04-07 09:01:06 +0300

    added the active btn using js

---
+++ app.js
@@ -0,0 +1,17 @@
+const section = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function pageTransition(){
+    for(let i = 0 ; i< sectBtn.length ; i++){
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn'
+
+        })
+    }
+}
+pageTransition();
